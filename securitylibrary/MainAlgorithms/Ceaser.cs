51bceb962f834a5ab655b581887143b27c5a7695
nada263 <nada263>
2022-03-09 07:45:01 +0200

    Implement Ceaser Algorithm

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -8,20 +8,76 @@ namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
+        char[] alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
-            
+            char[] input = plainText.ToUpper().ToCharArray();
+            string encrypted = "";
+            int index = 0;
+            foreach (char alphabet in input)
+            {
+                for (int i = 0; i < alphabets.Length; i++)
+                {
+                    if (alphabet.Equals(alphabets[i]))
+                    {
+                        index = i; break;
+                    }
+                }
+                index += key;
+                if (index < 0) index = 26 + index;
+                index %= 26;
+
+                encrypted += alphabets[index];
+            }
+            return encrypted;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            char[] input = cipherText.ToCharArray();
+            string decrypted = "";
+            int index = 0;
+            foreach (char alphabet in input)
+            {
+                for (int i = 0; i < alphabets.Length; i++)
+                {
+                    if (alphabet.Equals(alphabets[i]))
+                    {
+                        index = i; break;
+                    }
+                }
+                index -= key;
+                if (index < 0) index = 26 + index; 
+                index %= 26;
+                decrypted += alphabets[index];
+            }
+            return decrypted.ToLower();
         }
 
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            char PT = plainText.ToUpper().ToCharArray()[0];
+            char CT = cipherText.ToUpper().ToCharArray()[0];
+            int index_PT = 0, index_CT = 0;
+            int key;
+            for (int i = 0; i < alphabets.Length; i++)
+            {
+                if (PT.Equals(alphabets[i]))
+                {
+                    index_PT = i; break;
+                }
+            }
+            for (int i = 0; i < alphabets.Length; i++)
+            {
+                if (CT.Equals(alphabets[i]))
+                {
+                    index_CT = i; break;
+                }
+            }
+            key = index_CT - index_PT;
+            if (key < 0) key = 26 + key;
+            key %= 26;
+            return key;
         }
     }
 }
