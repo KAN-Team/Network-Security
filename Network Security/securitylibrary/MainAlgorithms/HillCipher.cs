917ec4180afd9025e708959532c5609fc18cdd63
Kareem100 <KareemSherif210@gmail.com>
2022-03-15 00:38:38 +0200

    Implementing HillCipher
    finished HillCipher Decryption methods both normal and bonus ones

--- Network Security/securitylibrary/MainAlgorithms/HillCipher.cs
+++ Network Security/securitylibrary/MainAlgorithms/HillCipher.cs
@@ -13,42 +13,72 @@ namespace SecurityLibrary
     {
         public List<int> Encrypt(List<int> plainText, List<int> key)
         {
-            // return cipher as an indexed letters.
+            // **** return cipher as an indexed letters **** //
             return doEncryption(plainText, key);
         }
 
         public string Encrypt(string plainText, string key)
         {
-            // **** return cipher as a string **** //
-
+            // **** this method returns cipher as a string **** //
+            // =============================================== //
             List<int> plainNumeric = new List<int>(new int[plainText.Length]);
             List<int> keyNumeric = new List<int>(new int[key.Length]);
-            plainText.ToLower();
-            key.ToLower();
+            plainText = plainText.ToLower();
+            key = key.ToLower();
+            // =============================================== //
 
+            // ======= Convert from letters to numbers ======= //
             for (int i = 0; i < plainText.Length; ++i)
                 plainNumeric[i] = plainText[i] - 'a';
 
             for (int i = 0; i < key.Length; ++i)
                 keyNumeric[i] = key[i] - 'a';
+            // =============================================== //
 
             List<int> cipherNumeric = doEncryption(plainNumeric, keyNumeric);
 
+            // ======= Convert from numbers to letters ======= //
             StringBuilder cipherText = new StringBuilder(cipherNumeric.Count + 1);
             for (int i = 0; i < cipherNumeric.Count; ++i)
                 cipherText.Append((char)('a' + cipherNumeric[i]));
+            // =============================================== //
 
             return cipherText.ToString();
         }
 
         public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            // **** return plain as an indexed letters **** //
+            return doDecryption(cipherText, key);
         }
 
-        public string Decrypt(string plainText, string key)
+        public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            // **** this method returns plain as a string **** //
+            // =============================================== //
+            List<int> cipherNumeric = new List<int>(new int[cipherText.Length]);
+            List<int> keyNumeric = new List<int>(new int[key.Length]);
+            cipherText = cipherText.ToLower();
+            key = key.ToLower();
+            // =============================================== //
+
+            // ======= Convert from letters to numbers ======= //
+            for (int i = 0; i < cipherText.Length; ++i)
+                cipherNumeric[i] = cipherText[i] - 'a';
+
+            for (int i = 0; i < key.Length; ++i)
+                keyNumeric[i] = key[i] - 'a';
+            // =============================================== //
+
+            List<int> plainNumeric = doDecryption(cipherNumeric, keyNumeric);
+
+            // ======= Convert from numbers to letters ======= //
+            StringBuilder plainText = new StringBuilder(plainNumeric.Count + 1);
+            for (int i = 0; i < plainNumeric.Count; ++i)
+                plainText.Append((char)('a' + plainNumeric[i]));
+            // =============================================== //
+
+            return plainText.ToString();
         }
 
         public List<int> Analyse(List<int> plainText, List<int> cipherText)
@@ -95,6 +125,120 @@ namespace SecurityLibrary
             }
 
             return cipherText;
+        }
+
+        private List<int> doDecryption(List<int> cipherText, List<int> key)
+        {
+            // =============================================== //
+            // Convert row based key into a 2D matrix
+            int N = (int)Math.Sqrt(key.Count);
+            int[,] keyMat = new int[N, N];
+            int c = 0;
+            for (int i = 0; i < N; ++i)
+                for (int j = 0; j < N; ++j)
+                    keyMat[i, j] = key[c++];
+            // =============================================== //
+
+            int[,] keyInverseMat = getKeyInverse(keyMat);
+
+            // =============================================== //
+            // Multiply the Key Inverse Matrix
+            // with the row based cipher List
+            List<int> numericalPlain = new List<int>(new int[cipherText.Count]);
+            int p = 0;
+            for (int m = 0; m < cipherText.Count;)
+            {
+                for (int i = 0; i < keyInverseMat.GetLength(0); ++i)
+                {
+                    for (int j = 0; j < keyInverseMat.GetLength(1); ++j)
+                        numericalPlain[p] += keyInverseMat[i, j] * cipherText[m + j];
+
+                    numericalPlain[p] %= 26;
+                    if (numericalPlain[p] < 0)
+                        numericalPlain[p] += 26;
+
+                    p++;
+                }
+                m += N;
+            }
+            // =============================================== //
+
+            return numericalPlain;
+        }
+
+        private int[,] getKeyInverse(int[,] keyMat)
+        {
+            if (keyMat.GetLength(0) == 2) // if the key is a 2D matrix
+            {
+                int a = keyMat[0, 0], b = keyMat[0, 1], c = keyMat[1, 0], d = keyMat[1, 1];
+                decimal factor = 1 / (a * d - b * c);       // determinant multiplicative inverse
+                a = (int)(Math.Ceiling(a * factor)) % 26;
+                b = (int)(Math.Ceiling(b * factor)) % 26;
+                c = (int)(Math.Ceiling(c * factor)) % 26;
+                d = (int)(Math.Ceiling(d * factor)) % 26;
+                keyMat[0, 0] = d; keyMat[1, 1] = a; keyMat[0, 1] = -b; keyMat[1, 0] = -c;
+
+                return keyMat;
+            }
+
+            // ================================================== //
+            // Get the Key Inverse of a 3D Matrix
+            int det = getKeyDeterminant(keyMat);
+
+            int detInv = getDeterminantInverse(det);
+
+            int[,] keyInverseMat = new int[keyMat.GetLength(0), keyMat.GetLength(1)];
+            for (int i = 0; i < keyMat.GetLength(0); ++i)
+            {
+                for (int j = 0; j < keyMat.GetLength(1); ++j)
+                {
+                    int temp1 = detInv * (int)Math.Pow(-1, i + j);
+                    int temp2 = getSubDeterminant(keyMat, i, j) % 26; // may be a negative value
+                    keyInverseMat[i, j] = ((temp1 * temp2) % 26 + 26) % 26;
+                }
+            }
+
+            // transpose the matrix
+            for (int i = 0; i < keyMat.GetLength(0); ++i)
+                for (int j = 0; j < keyMat.GetLength(1); ++j)
+                    keyMat[i, j] = keyInverseMat[j, i];
+            // ================================================== //
+
+            return keyMat;
+        }
+
+        private int getKeyDeterminant(int[,] keyMat)
+        {
+            int firstPart = keyMat[0, 0] * (keyMat[1, 1] * keyMat[2, 2] - keyMat[1, 2] * keyMat[2, 1]);
+            int secondPart = keyMat[0, 1] * (keyMat[1, 0] * keyMat[2, 2] - keyMat[1, 2] * keyMat[2, 0]);
+            int thirdPart = keyMat[0, 2] * (keyMat[1, 0] * keyMat[2, 1] - keyMat[1, 1] * keyMat[2, 0]);
+
+            return ((firstPart - secondPart + thirdPart) % 26 + 26) % 26;
+        }
+
+        private int getDeterminantInverse(int det)
+        {
+            // calculate the determinant multiplicative inverse
+            int b = 1, R = 26 - det;
+            while (true)
+                if (b % R == 0) break;
+                else b += 26;
+            return 26 - (b / R);
+        }
+
+        private int getSubDeterminant(int[,] mat, int r, int c)
+        {
+            List<int> region = new List<int>();
+            for (int i = 0; i < 3; ++i)
+            {
+                if (i == r) continue;
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (j == c) continue;
+                    region.Add(mat[i, j]);
+                }
+            }
+            return region[0] * region[3] - region[1] * region[2];
         }
         #endregion
     }
