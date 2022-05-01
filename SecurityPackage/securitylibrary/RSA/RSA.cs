ea54ebdc9937c2c1c9d90af8f761ba5f532235ec
kareem saeed <karimsaid026@gmail.com>
2022-05-01 05:05:03 +0200

    Updating RSA Algorithm and Tests

--- SecurityPackage/securitylibrary/RSA/RSA.cs
+++ SecurityPackage/securitylibrary/RSA/RSA.cs
@@ -12,40 +12,36 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            BigInteger encrypted = 1;
             int n = p * q;
 
-            while (e > 3)
-            {
-                encrypted *= (int)(Math.Pow(M, 3) % n);
-                e -= 3;
-            }
-
-            encrypted *= (int)(Math.Pow(M, e) % n);
-            encrypted %= n;
-
-            return (int)encrypted;
-
+            return getPowMod(M, e, n);
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            BigInteger decrypted = 1;
             int n = p * q;
             int phayN = (p - 1) * (q - 1);
             ExtendedEuclid obj = new ExtendedEuclid();
             int d = obj.GetMultiplicativeInverse(e, phayN);
 
-
-            while (d > 3)
-            {
-                decrypted *= (int)(Math.Pow(C, 3) % n);
-                d -= 3;
-            }
-            decrypted *= (int)(Math.Pow(C, d) % n);
-            decrypted %= n;
-
-            return (int)decrypted;
+            return getPowMod(C, d, n);
         }
+
+
+        #region HELPERS
+        private int getPowMod(int msg, int k, int n)
+        {
+            BigInteger newMsg = 1;
+            while (k > 3)
+            {
+                newMsg *= (int)(Math.Pow(msg, 3) % n);
+                k -= 3;
+            }
+            newMsg *= (int)(Math.Pow(msg, k) % n);
+            newMsg %= n;
+
+            return (int)newMsg;
+        }
+        #endregion
     }
 }
