c1fb8ec8671b3c2ea80e8a9ec11e8fac8efc7963
kareem saeed <karimsaid026@gmail.com>
2022-05-01 02:15:55 +0200

    RSA Alogrithm

--- SecurityPackage/securitylibrary/RSA/RSA.cs
+++ SecurityPackage/securitylibrary/RSA/RSA.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SecurityLibrary.AES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,40 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            BigInteger encrypted = 1;
+            int n = p * q;
+
+            while (e > 3)
+            {
+                encrypted *= (int)(Math.Pow(M, 3) % n);
+                e -= 3;
+            }
+
+            encrypted *= (int)(Math.Pow(M, e) % n);
+            encrypted %= n;
+
+            return (int)encrypted;
+
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            BigInteger decrypted = 1;
+            int n = p * q;
+            int phayN = (p - 1) * (q - 1);
+            ExtendedEuclid obj = new ExtendedEuclid();
+            int d = obj.GetMultiplicativeInverse(e, phayN);
+
+
+            while (d > 3)
+            {
+                decrypted *= (int)(Math.Pow(C, 3) % n);
+                d -= 3;
+            }
+            decrypted *= (int)(Math.Pow(C, d) % n);
+            decrypted %= n;
+
+            return (int)decrypted;
         }
     }
 }
