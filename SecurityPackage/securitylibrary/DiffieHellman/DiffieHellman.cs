346c530eded23f2a5353df9588dca03c8e0b29c2
kareem saeed <karimsaid026@gmail.com>
2022-05-01 06:14:43 +0200

    Adding DiffieHellman Algorithm and El Gamal Algorithm with it's tests

--- SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,35 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> secretKeys = new List<int>();
+
+            int Ya = getPowMod(alpha, xa, q);
+            int Yb = getPowMod(alpha, xb, q);
+
+            int K1 = getPowMod(Yb, xa, q);
+            int K2 = getPowMod(Ya, xb, q);
+
+            secretKeys.Add(K1);
+            secretKeys.Add(K2);
+
+            return secretKeys;
         }
+
+
+        #region HELPERS
+        private int getPowMod(int msg, int k, int n)
+        {
+            BigInteger newMsg = 1;
+            while (k > 3)
+            {
+                newMsg *= (int)(Math.Pow(msg, 3) % n);
+                k -= 3;
+            }
+            newMsg *= (int)(Math.Pow(msg, k) % n);
+            newMsg %= n;
+
+            return (int)newMsg;
+        }
+        #endregion
     }
 }
